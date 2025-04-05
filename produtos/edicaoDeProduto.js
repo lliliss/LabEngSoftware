8c22bd443e2a36eeac8591d32900d4f61da0403e
thaynacdts <thaynacdts@gmail.com>
2025-04-05 12:33:39 -0300

    alterações no sidebar e produtos quase pronto

---
+++ produtos/edicaoDeProduto.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const botaoVoltar = document.getElementById("Voltar");
+
+    if (botaoVoltar) {
+        botaoVoltar.addEventListener("click", function() {
+            window.location.href = "produtos.html"; 
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const botaoVoltar = document.getElementById("voltar");  
+
+    if (botaoVoltar) {
+        botaoVoltar.addEventListener("click", function() {
+            window.location.href = "produtos.html"; 
+        });
+    }
+});
+
+
+function mensagemSalvo(texto, cor) {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = texto;  
+    mensagem.style.backgroundColor = cor;  
+    mensagem.style.display = "block";  
+
+    
+    setTimeout(() => {
+        mensagem.style.display = "none";  
+    }, 3000);
+}
+
+
+document.getElementById("salvar").addEventListener("click", function() {
+    mensagemSalvo("Produto salvo com sucesso!", "#4CAF50"); 
+});
+
+
+function mensagemExcluir(texto, cor) {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = texto;  
+    mensagem.style.backgroundColor = cor;  
+    mensagem.style.display = "block";  
+
+    setTimeout(() => {
+        mensagem.style.display = "none";  
+    }, 3000);
+}
+
+
+document.getElementById("excluir").addEventListener("click", function() {
+    mensagemExcluir("Produto excluído com sucesso!", "#DC3545"); 
+});
