5afd15b459a32d664565131c9d627f0264af8dbe
unknown <opedrosantos16@gmail.com>
2025-06-10 20:30:03 -0300

    link teste 2

--- public/login/logins.js
+++ public/login/logins.js
@@ -7,7 +7,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
   mensagemErro.textContent = '';
   
   try {
-    const response = await fetch('http://localhost:5000/api/login', {
+    const response = await fetch('https://pharmasafe.vercel.app/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, senha })
